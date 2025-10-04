88d7a7bf346f43ca95eaadc01fcb40e7b4b3121a
Raj Kiran <raj.kiran.n@oracle.com>
2025-10-04 23:04:28 +0530

    added psycopg2 db adaptor for postgres

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """
+    adds two numbers x and y and returns result
+    """
+    return x + y
+
+def subtract(x,y):
+    """
+    subtract x from y and returns result
+    """
+    return y - x 
